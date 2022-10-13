9db7b8ba088adbe5a0569b99fa613b3e7c1b1741
CalebWAC <calebwac@outlook.com>
2022-10-12 18:08:42 -0600

    conflict

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -5,11 +5,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
-<<<<<<< HEAD
-import java.otherTHing.*;
-=======
-import java.util.*;
->>>>>>> e8609e31b78a62c1df352e4f466f0c6bb09ae094
 
 /**
  * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
